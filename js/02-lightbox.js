8b6826e2615b1a50529c4f1c6c7325be047cb26b
Aleksandr-Kovshar <aleksandr.mpro@gmail.com>
2022-08-31 00:35:34 +0300

    7/2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+function creatGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+const itemsMarkup = creatGalleryItemsMarkup(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+// galleryList.innerHTML = creatGalleryItemsMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', itemsMarkup);
+
+
+  
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
